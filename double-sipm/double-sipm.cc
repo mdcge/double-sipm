1359ac291f976ceb50d3eeb9c2c9e1b3d088f35c
Max Generowicz <git-mdcg@my-post-office.net>
2023-07-07 19:11:50 +0200

    Demonstrate user actions without classes

--- double-sipm/double-sipm.cc
+++ double-sipm/double-sipm.cc
@@ -16,6 +16,8 @@
 #include <G4RandomDirection.hh>
 #include <G4ThreeVector.hh>
 #include <G4Tubs.hh>
+#include <G4Run.hh>
+#include <G4Step.hh>
 
 #include <G4UIExecutive.hh>
 #include <G4UImanager.hh>
@@ -25,9 +27,21 @@
 #include <memory>
 
 
+void xxx (G4double& total_edep, G4Step const* step) {
+    G4double pre_energy = step -> GetPreStepPoint() -> GetTotalEnergy();
+    G4double post_energy = step -> GetPostStepPoint() -> GetTotalEnergy();
+    total_edep += pre_energy - post_energy;
+}
+
 int main(int argc, char *argv[]) {
 
     auto two_gammas = [](auto event){ generate_back_to_back_511_keV_gammas(event, {}, 0); };
+
+    G4double total_edep = 0;
+
+    auto run_action_end = [&total_edep] (G4Run const* run) { G4cout << "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX " << total_edep << G4endl; };
+    // This can be used to move the closure "out of scope" (xxx is outside of main)
+    auto step_action = [&total_edep] (G4Step const* step) { xxx(total_edep, step); };
 
     G4int verbosity = 0;
     auto physics_list = new FTFP_BERT{verbosity};
@@ -38,11 +52,11 @@ int main(int argc, char *argv[]) {
         {G4RunManagerFactory::CreateRunManager(G4RunManagerType::Serial)};
 
     // Physics list must be attached to run manager before instantiating other user action classes
-    //auto make_actions = new n4::actions{two_gammas};
     run_manager -> SetUserInitialization(physics_list);
     run_manager -> SetUserInitialization((new n4::actions{two_gammas})
                                          -> set((new n4::run_action())
-                                                -> end([] (G4Run const* run) {G4cout << "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" << G4endl;})));
+                                                -> end(run_action_end))
+                                         -> set((new n4::stepping_action{step_action})));
     run_manager -> SetUserInitialization(new n4::geometry{make_geometry});
 
 
