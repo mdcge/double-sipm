94a62a7aa940ef1accedc8503d6f959c5597e7c8
Max Generowicz <git-mdcg@my-post-office.net>
2023-07-12 16:49:20 +0200

    Change generator to emulate non-point-like gamma emission

--- double-sipm/generator.cc
+++ double-sipm/generator.cc
@@ -11,8 +11,15 @@
 
 void generate_back_to_back_511_keV_gammas(G4Event* event, G4ThreeVector position, G4double time) {
     auto gamma = n4::find_particle("gamma");
-    auto direction = G4ThreeVector(3*G4UniformRand()-1.5, 3*G4UniformRand()-1.5, 12.5).unit(); // random unit vector which hits scintillator
+
+    G4double source_radius = 1.5*mm;
+    position = G4RandomDirection() * source_radius * G4UniformRand();
+    position.setX(0);
+
+    G4double y_offset = 3*mm + position.getY();
+    auto direction = G4ThreeVector(3*mm * G4UniformRand() - 1.5*mm, y_offset * G4UniformRand() - (y_offset/2), 12.5).unit(); // random unit vector which hits scintillator
     auto p = 0.511*MeV * direction;
+
     auto vertex = new G4PrimaryVertex(position, time);
     vertex -> SetPrimary(new G4PrimaryParticle(gamma,  p.x(),  p.y(),  p.z()));
     vertex -> SetPrimary(new G4PrimaryParticle(gamma, -p.x(), -p.y(), -p.z()));
