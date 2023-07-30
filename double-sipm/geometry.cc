4b336bcc524ef3bac531ea90b4e613546064460a
Jacek Generowicz <jacg@my-post-office.net>
2023-07-30 18:01:51 +0200

    Factor material properties out of make_geometry

--- double-sipm/geometry.cc
+++ double-sipm/geometry.cc
@@ -1,4 +1,5 @@
 #include "geometry.hh"
+#include "materials.hh"
 
 #include "nain4.hh"
 #include "n4-volumes.hh"
@@ -28,70 +29,14 @@ using vec_double = std::vector<G4double>;
 using vec_int    = std::vector<G4int>;
 
 const G4double hc = CLHEP::h_Planck * CLHEP::c_light;
-const vec_double OPTPHOT_ENERGY_RANGE{1*eV, 8.21*eV};
 
 G4double detection_probability(G4double energy, std::vector<G4double>& energies, std::vector<G4double>& scintillation);
 
-G4Material* csi_with_properties() {
-    auto csi = n4::material("G4_CESIUM_IODIDE");
-    // csi_rindex: values taken from "Optimization of Parameters for a CsI(Tl) Scintillator Detector Using GEANT4-Based Monte Carlo..." by Mitra et al (mainly page 3)
-    //  csi_scint: Fig. 2 in the paper
-    // must be in increasing ENERGY order (decreasing wavelength) for scintillation to work properly
-    auto     csi_energies = n4::scale_by(hc*eV, {1/0.9, 1/0.7, 1/0.54, 1/0.35}); // denominator is wavelength in micrometres
-    vec_double csi_rindex =                     {1.79 , 1.79 , 1.79  , 1.79  };  //vec_double csi_rindex = {2.2094, 1.7611};
-    vec_double  csi_scint =                     {0.0  , 0.1  , 1.0   , 0.0   };
-    auto    csi_abslength = n4::scale_by(m    , {5    , 5    , 5     , 5     });
-    // Values from "Temperature dependence of pure CsI: scintillation light yield and decay time" by Amsler et al
-    // "cold" refers to ~77K, i.e. liquid nitrogen temperature
-    G4double csi_scint_yield      =  3200 / MeV;
-    G4double csi_scint_yield_cold = 50000 / MeV;
-    G4double csi_time_fast        =     6 * ns;
-    G4double csi_time_slow        =    28 * ns;
-    G4double csi_time_fast_cold   =  1015 * ns; // only one component at cold temps!
-    G4double csi_time_slow_cold   =  1015 * ns;
-    G4MaterialPropertiesTable *csi_mpt = n4::material_properties()
-        .add("RINDEX"                 , csi_energies, csi_rindex)
-        .add("SCINTILLATIONCOMPONENT1", csi_energies,  csi_scint)
-        .add("SCINTILLATIONCOMPONENT2", csi_energies,  csi_scint)
-        .add("ABSLENGTH"              , csi_energies, csi_abslength)
-        .add("SCINTILLATIONTIMECONSTANT1", csi_time_fast)
-        .add("SCINTILLATIONTIMECONSTANT2", csi_time_slow)
-        .add("SCINTILLATIONYIELD"        , csi_scint_yield)
-      //.add("SCINTILLATIONYIELD"        ,   100 / MeV) // for testing
-        .add("SCINTILLATIONYIELD1"       ,     0.57   )
-        .add("SCINTILLATIONYIELD2"       ,     0.43   )
-        .add("RESOLUTIONSCALE"           ,     1.0    )
-        .done();
-    csi -> GetIonisation() -> SetBirksConstant(0.00152 * mm/MeV);
-    csi -> SetMaterialPropertiesTable(csi_mpt);
-    return csi;
-}
-
-G4Material* air_with_properties() {
-    auto air = n4::material("G4_AIR");
-    G4MaterialPropertiesTable *mpt_air = n4::material_properties()
-        .add("RINDEX", OPTPHOT_ENERGY_RANGE, {1, 1})
-        .done();
-    air -> SetMaterialPropertiesTable(mpt_air);
-    return air;
-}
-
-G4Material* teflon_with_properties() {
-    auto teflon = n4::material("G4_TEFLON");
-    // Values could be taken from "Optical properties of Teflon AF amorphous fluoropolymers" by Yang, French & Tokarsky (using AF2400, Fig.6)
-    // but are also stated in the same paper as above
-    G4MaterialPropertiesTable *mpt_teflon = n4::material_properties()
-        .add("RINDEX", OPTPHOT_ENERGY_RANGE, {1.35, 1.35})
-        .done();
-    teflon -> SetMaterialPropertiesTable(mpt_teflon);
-    return teflon;
-}
 
 G4PVPlacement* make_geometry(vec_int& photon_count, std::vector<std::vector<G4double>>& times_of_arrival) {
     auto csi    =    csi_with_properties();
     auto air    =    air_with_properties();
     auto teflon = teflon_with_properties();
-
     auto plastic = n4::material("G4_POLYCARBONATE"); // probably wrong
 
     G4double scint_xy = 3*mm, scint_z = 2*cm;
---
+++ double-sipm/materials.hh
@@ -0,0 +1,10 @@
+#ifndef MATERIALS_HH
+#define MATERIALS_HH
+
+#include <G4Material.hh>
+
+G4Material*    csi_with_properties();
+G4Material*    air_with_properties();
+G4Material* teflon_with_properties();
+
+#endif // MATERIALS_HH
