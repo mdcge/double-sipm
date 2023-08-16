a1ce1a0a56bad1942c7ce24e61804479993d71f6
Max Generowicz <git-mdcg@my-post-office.net>
2023-08-16 16:28:21 +0200

    Add gamma interaction z position function

--- double-sipm/double-sipm.cc
+++ double-sipm/double-sipm.cc
@@ -43,8 +43,25 @@ G4double delta_total_energy(G4Step const * step) {
 // Add energies deposited in this step to running totals of deposited energies in whole event
 void add_step_edep(std::vector<G4double>& total_edep, G4Step const* step) {
     auto step_solid_name = step -> GetPreStepPoint() -> GetTouchable() -> GetVolume() -> GetName();
-    if      (step_solid_name == "Scintillator-0") { total_edep[0] += delta_total_energy(step); }
-    else if (step_solid_name == "Scintillator-1") { total_edep[1] += delta_total_energy(step); }
+    auto particle_name = step -> GetTrack() -> GetDefinition() -> GetParticleName();
+    auto particle_pos = step -> GetTrack() -> GetPosition();
+    auto interaction = step -> GetPostStepPoint() -> GetProcessDefinedStep() -> GetProcessName();
+    // if (delta_total_energy(step) > 0) { G4cout << "Particle " << particle_name << " (Track ID: " << step -> GetTrack() -> GetTrackID() << ", Parent ID: " << step -> GetTrack() -> GetParentID() << ") " << " deposited " << delta_total_energy(step) << " energy, " << " process: " << interaction << G4endl; }
+    if (step_solid_name == "Scintillator") {
+        if      (particle_pos.z() > 0) { total_edep[0] += delta_total_energy(step); }
+        else if (particle_pos.z() < 0) { total_edep[1] += delta_total_energy(step); }
+    }
+}
+
+void gamma_interaction_z_pos(std::vector<std::vector<G4double>>& gamma_zs, G4Step const* step) {
+    auto step_solid_name = step -> GetPreStepPoint() -> GetTouchable() -> GetVolume() -> GetName();
+    auto particle_name = step -> GetTrack() -> GetDefinition() -> GetParticleName();
+    auto particle_pos = step -> GetTrack() -> GetPosition();
+    auto interaction = step -> GetPostStepPoint() -> GetProcessDefinedStep() -> GetProcessName();
+    if ((particle_name == "gamma") && (step_solid_name == "Scintillator") && ((interaction == "compt") || (interaction == "phot"))) {
+        if (particle_pos.z() > 0) { gamma_zs[0].push_back(particle_pos.z()); }
+        else { gamma_zs[1].push_back(particle_pos.z()); };
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -54,48 +71,82 @@ int main(int argc, char *argv[]) {
 
     // Open output file at start of run, close it at the end of the run
 
-    std::ofstream data_file_0; std::ofstream data_file_1;
-    std::vector<std::ofstream*> data_file{&data_file_0, &data_file_1};
-    auto open_file = [&data_file] (G4Run const*) {
-        data_file[0] -> open("G4_photon_times_0.csv");
-        data_file[1] -> open("G4_photon_times_1.csv");
+    std::ofstream gamma_z_data_file_0; std::ofstream gamma_z_data_file_1;
+    std::vector<std::ofstream*> gamma_z_data_files{&gamma_z_data_file_0, &gamma_z_data_file_1};
+    std::ofstream time_data_file_0; std::ofstream time_data_file_1;
+    std::vector<std::ofstream*> time_data_files{&time_data_file_0, &time_data_file_1};
+    std::ofstream edep_data_file_0; std::ofstream edep_data_file_1;
+    std::vector<std::ofstream*> edep_data_files{&edep_data_file_0, &edep_data_file_1};
+    auto open_file = [&gamma_z_data_files, &time_data_files, &edep_data_files] (G4Run const*) {
+        gamma_z_data_files[0] -> open("z_pos_0.csv");
+        gamma_z_data_files[1] -> open("z_pos_1.csv");
+        time_data_files[0] -> open("times_0.csv");
+        time_data_files[1] -> open("times_1.csv");
+        edep_data_files[0] -> open("edeps_0.csv");
+        edep_data_files[1] -> open("edeps_1.csv");
     };
-    auto close_file = [&data_file] (G4Run const*) { data_file[0] -> close(); data_file[1] -> close(); };
+    auto close_file = [&gamma_z_data_files, &time_data_files, &edep_data_files] (G4Run const*) {
+        gamma_z_data_files[0] -> close();
+        gamma_z_data_files[1] -> close();
+        time_data_files[0] -> close();
+        time_data_files[1] -> close();
+        edep_data_files[0] -> close();
+        edep_data_files[1] -> close();
+    };
 
     // Accumulators for energy and photons observed in each scintillator during a single event
     std::vector<G4double> total_edep{0, 0};
+    std::vector<std::vector<G4double>> gamma_zs{{}, {}};
     std::vector<std::vector<G4double>> times_of_arrival{{}, {}};
     // At the start of each event: reset the accumulators to zero
-    auto reset_photon_count = [&total_edep, &times_of_arrival] (G4Event const*) {
-        total_edep  [0] = total_edep  [1] = 0;
+    auto reset_photon_count = [&gamma_zs, &total_edep, &times_of_arrival] (G4Event const*) {
+        total_edep[0] = total_edep[1] = 0;
+        gamma_zs[0].clear();
+        gamma_zs[1].clear();
         times_of_arrival[0].clear();
         times_of_arrival[1].clear();
     };
 
     G4int double_hits = 0;
-    auto write_photon_count = [&data_file, &double_hits, &total_edep, &times_of_arrival] (G4Event const* event) {
-        //G4cout << "Event number: " << event -> GetEventID() << G4endl;
-        // G4cout << G4endl << "Total deposited energy in scintillator 0: " << total_edep_0 << G4endl;
-        // G4cout << "Total deposited energy in scintillator 1: " << total_edep_1 << G4endl;
-        // G4cout << "Photon count 0: " << photon_count[0] << G4endl;
-        // G4cout << "Photon count 1: " << photon_count[1] << G4endl << G4endl;
+    auto write_photon_count = [&gamma_z_data_files, &time_data_files, &edep_data_files, &double_hits, &gamma_zs, &total_edep, &times_of_arrival] (G4Event const* event) {
 
-        G4cout << "Number of double events: " << double_hits << "/" << event -> GetEventID() << " events" << G4endl;
-        // G4cout << "Number of times: " << times_of_arrival[0].size() << " and " << times_of_arrival[1].size() << G4endl;
-        std::vector<size_t> photon_count{times_of_arrival[0].size(), times_of_arrival[0].size()};
+        G4int event_nb = event -> GetEventID() + 1;
+        if (event_nb % 100 == 0) {
+            G4cout << "Number of double events: " << double_hits << "/" << event_nb << " events" << G4endl;
+        }
+
+        // Writing the photon count
         std::vector<size_t> photon_count{times_of_arrival[0].size(), times_of_arrival[1].size()};
         if (photon_count[0] > 0 && photon_count[1] > 0) { double_hits += 1; }
         for (int side=0; side < 2; ++side) {
             for (size_t i=0; i<times_of_arrival[side].size(); i++) {
-                if (photon_count[side] != 0) { *data_file[side] << times_of_arrival[side][i] << ","; }
-                else                         { *data_file[side] << 0; }
+                if (photon_count[side] != 0) { *time_data_files[side] << times_of_arrival[side][i] << ","; }
+                else                         { *time_data_files[side] << 0; }
             }
-            *data_file[side] << std::endl;
+            *time_data_files[side] << std::endl;
         }
+
+        // Writing the gamma interaction z position
+        for (int side=0; side < 2; ++side) {
+            if (gamma_zs[side].size() == 0) { *gamma_z_data_files[side] << 0 << ","; }
+            for (int i=0; i < gamma_zs[side].size(); i++) {
+                *gamma_z_data_files[side] << gamma_zs[side][i] << ",";
+            }
+            *gamma_z_data_files[side] << std::endl;
+        }
+
+        // Writing the deposited energy
+        // G4cout << "Energies: " << total_edep[0] << ", " << total_edep[1] << G4endl;
+        // G4cout << "Counts: " << photon_count[0] << ", " << photon_count[1] << G4endl;
+        *edep_data_files[0] << total_edep[0] << ",";
+        *edep_data_files[1] << total_edep[1] << ",";
     };
 
     // At every step: increment running total of deposited energy during the event
-    auto accumulate_energy = [&total_edep] (G4Step const* step) { add_step_edep(total_edep, step); };
+    auto accumulate_energy = [&gamma_zs, &total_edep] (G4Step const* step) {
+        add_step_edep(total_edep, step);
+        gamma_interaction_z_pos(gamma_zs, step);
+    };
 
     // If needed, use this as stacking_action -> classify, to disable tracking
     // of scintillation products
